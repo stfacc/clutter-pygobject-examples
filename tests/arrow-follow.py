89cb4bb1df07a5f7cc946ad4aec38996997a0c91
Jeremy Moles <cubicool@gmail.com>
2011-12-11 14:17:26 -0500

    removes arrow-follow.py's usage of cairo.

--- tests/arrow-follow.py
+++ tests/arrow-follow.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+# Example by: Jeremy Moles <cubicool@gmail.com>
 
+import sys
 import math
-import cairo
 
 from gi.repository import Clutter, Cogl
 
@@ -44,22 +45,17 @@ class Arrow(Clutter.Actor):
         l2  = length(v2)
         n2  = v2[0] / l2, v2[1] / l2
         deg = angle(v1, n2) * (180.0 / math.pi)
-        
+
         if px < 0.0:
             deg = -deg
 
         self.set_z_rotation_from_gravity(deg, Clutter.Gravity.CENTER)
 
 def motion_event(stage, event, arrow):
-    x,  y  = arrow.get_position()
-    w,  h  = arrow.get_size()
-    tx, ty = -(x + (w / 2.0)), -(y + (h / 2.0))
-    mat    = cairo.Matrix()
-
-    mat.scale(1.0, -1.0)
-    mat.translate(tx, ty)
-
-    arrow.point_towards(*mat.transform_point(event.x, event.y))
+    x, y = arrow.get_position()
+    w, h = arrow.get_size()
+    
+    arrow.point_towards(event.x - (x + (w / 2.0)), -event.y + (y + (h / 2.0)))
 
 def press_event(stage, event, arrow):
     arrow.move_towards(event.x, event.y)
